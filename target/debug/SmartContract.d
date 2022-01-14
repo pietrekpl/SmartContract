d888f5d3634bcbd377ce63c58797b4df907d35c3
Piotr Ludynia <pietrekpl18@gmail.com>
2022-01-14 15:43:10 +0100

    Initial Commit

---
+++ target/debug/SmartContract.d
@@ -0,0 +1,1 @@
+C:\Users\Lenovo\projects\SmartContract\target\debug\SmartContract.exe: C:\Users\Lenovo\projects\SmartContract\src\main.rs
---
+++ target/debug/deps/SmartContract.d
@@ -0,0 +1,5 @@
+C:\Users\Lenovo\projects\SmartContract\target\debug\deps\SmartContract.exe: src\main.rs
+
+C:\Users\Lenovo\projects\SmartContract\target\debug\deps\SmartContract.d: src\main.rs
+
+src\main.rs:
